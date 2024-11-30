f0e4a93aa2c26b7070587d19bbe060ccde420bc9
Nelson Marro <nelsonmarro99@gmail.com>
2024-11-30 00:50:04 -0500

    /home/nelson/.config/nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,14 +4,14 @@
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 local keymap = vim.keymap
 
-keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-keymap.set('n', 'x', '"_x')
+keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+keymap.set("n", "x", '"_x')
 
 -- Select All
-keymap.set('n', '<M-y>', 'ggVG', { desc = 'Select All' })
+keymap.set("n", "<M-y>", "ggVG", { desc = "Select All" })
 
 -- paste without overwriting
-keymap.set('v', 'p', 'P')
+keymap.set("v", "p", "P")
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -19,13 +19,13 @@ keymap.set('v', 'p', 'P')
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
-keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -36,35 +36,26 @@ keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 -- keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 -- keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-keymap.set('n', '<leader>th', "<Cmd>exe v:count1 . 'ToggleTerm direction=horizontal'<CR>", { desc = 'Toggle Terminal Horizontal' })
-keymap.set('n', '<leader>tv', "<Cmd>exe v:count1 . 'ToggleTerm direction=vertical size=70'<CR>", { desc = 'Toggle Terminal Vertical' })
+keymap.set(
+  "n",
+  "<leader>th",
+  "<Cmd>exe v:count1 . 'ToggleTerm direction=horizontal'<CR>",
+  { desc = "Toggle Terminal Horizontal" }
+)
+keymap.set(
+  "n",
+  "<leader>tv",
+  "<Cmd>exe v:count1 . 'ToggleTerm direction=vertical size=70'<CR>",
+  { desc = "Toggle Terminal Vertical" }
+)
 
-keymap.set('n', '<leader>tr', "<Cmd>exe v:count1 . 'ToggleTermSetName'<CR>", { desc = 'Set Name To Terminal' })
-keymap.set('n', '<leader>ta', '<Cmd>ToggleTermToggleAll<CR>', { desc = 'Toggle All Terminals' })
-keymap.set('n', '<leader>ts', '<Cmd>TermSelect<CR>', { desc = 'Select Terminal' })
-
--- noice
-keymap.set('c', '<S-Enter>', function()
-  require('noice').redirect(fn.getcmdline())
-end, { desc = 'Redirect Cmdline' })
-
-keymap.set({ 'n', 'i', 's' }, '<c-f>', function()
-  if not require('noice.lsp').scroll(4) then
-    return '<c-f>'
-  end
-end, { silent = true, expr = true })
-
-keymap.set({ 'n', 'i', 's' }, '<c-b>', function()
-  if not require('noice.lsp').scroll(-4) then
-    return '<c-b>'
-  end
-end, { silent = true, expr = true })
--- telescope
-vim.api.nvim_set_keymap('n', '<Leader>ff', '<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files,-u<CR>', { noremap = true, silent = true })
+keymap.set("n", "<leader>tr", "<Cmd>exe v:count1 . 'ToggleTermSetName'<CR>", { desc = "Set Name To Terminal" })
+keymap.set("n", "<leader>ta", "<Cmd>ToggleTermToggleAll<CR>", { desc = "Toggle All Terminals" })
+keymap.set("n", "<leader>ts", "<Cmd>TermSelect<CR>", { desc = "Select Terminal" })
 
 if vim.g.vscode then
-  keymap.set('n', '<leader>c', '<Cmd>e ~/.config/nvim/lua/config/keymaps.lua<CR>')
-  vim.keymap.set({ 'n', 'x', 'i' }, '<C-d>', function()
-    require('vscode-multi-cursor').addSelectionToNextFindMatch()
+  keymap.set("n", "<leader>c", "<Cmd>e ~/.config/nvim/lua/config/keymaps.lua<CR>")
+  vim.keymap.set({ "n", "x", "i" }, "<C-d>", function()
+    require("vscode-multi-cursor").addSelectionToNextFindMatch()
   end)
 end
