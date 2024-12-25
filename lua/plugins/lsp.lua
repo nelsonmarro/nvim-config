5ecc22244225b1f6f9bc3fb771a600351e65cc23
Nelson Marro <nelsonmarro99@gmail.com>
2024-12-25 11:20:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -25,7 +25,15 @@ return {
       },
       ---@type lspconfig.options
       servers = {
-        qmlls = {},
+        qmlls = {
+            cmd = { "qml" },
+            filetypes = { "qml", "qmljs" },
+            root_dir = function(fname)
+              return vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
+            end,
+            single_file_support = true,
+          },
+          },
         gopls = {
           settings = {
             gopls = {
