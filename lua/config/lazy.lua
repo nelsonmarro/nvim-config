a5b75411fc3a648146771d836c5204e0f00f994a
Nelson Marro <nelsonmarro99@gmail.com>
2024-12-27 12:50:00 -0500

    /home/nelson/.config/nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,7 +26,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "catppuccin" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true, concurrency = 5 }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
