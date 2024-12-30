7ace6421d36d4d57d37dd9448ec40f49fc3c4f39
Nelson Marro <nelsonmarro99@gmail.com>
2024-12-29 21:40:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,16 +1,16 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = false,
-        help = false,
-      },
-      copilot_node_command = "/home/nelson/.nvm/versions/node/v22.12.0/bin/node",
-    },
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   opts = {
+  --     suggestion = { enabled = false },
+  --     panel = { enabled = false },
+  --     filetypes = {
+  --       markdown = false,
+  --       help = false,
+  --     },
+  --     copilot_node_command = "/home/nelson/.nvm/versions/node/v22.12.0/bin/node",
+  --   },
+  -- },
 }
