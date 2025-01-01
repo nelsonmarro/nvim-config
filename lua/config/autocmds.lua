828d17459e08d7740a25181813a89d1a2381f198
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-01 12:10:00 -0500

    /home/nelson/.config/nvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,39 +3,39 @@
 -- Add any additional autocmds here
 
 local function augroup(name)
-  return vim.api.nvim_create_augroup('lazyvim_' .. name, { clear = true })
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
 
-vim.api.nvim_create_autocmd({ 'FileType' }, {
-  pattern = { 'cs' },
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "cs" },
   callback = function()
-    vim.keymap.set('n', '<leader>gd', function()
-      require('csharpls_extended').lsp_definitions()
+    vim.keymap.set("n", "<leader>gd", function()
+      require("csharpls_extended").lsp_definitions()
       return true
-    end, { desc = 'Go to definition csharp' })
+    end, { desc = "Go to definition csharp" })
   end,
 })
 
-vim.api.nvim_create_autocmd({ 'FileType' }, {
-  pattern = { 'go' },
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "go" },
   callback = function()
-    vim.keymap.set('n', '<leader>cge', '<cmd>GoIfErr<CR>', { desc = 'Add err check' })
-    vim.keymap.set('n', '<leader>cgl', '<cmd>GoLint<CR>', { desc = 'Lint go project' })
-    vim.keymap.set('n', '<leader>cgc', '<cmd>:lua require("go.comment").gen()<CR>', { desc = 'Comment go' })
+    vim.keymap.set("n", "<leader>cge", "<cmd>GoIfErr<CR>", { desc = "Add err check" })
+    vim.keymap.set("n", "<leader>cgl", "<cmd>GoLint<CR>", { desc = "Lint go project" })
+    vim.keymap.set("n", "<leader>cgc", '<cmd>:lua require("go.comment").gen()<CR>', { desc = "Comment go" })
   end,
 })
 
-vim.api.nvim_create_autocmd({ 'FileType' }, {
-  pattern = { 'markdown' },
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown" },
   callback = function()
-    vim.opt.colorcolumn = '80'
+    vim.opt.colorcolumn = "80"
   end,
 })
 
 -- wrap and check for spell in text filetypes
-vim.api.nvim_create_autocmd('FileType', {
-  group = augroup 'wrap_spell',
-  pattern = { 'gitcommit', 'markdown' },
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "gitcommit", "markdown" },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = false
@@ -44,14 +44,27 @@ vim.api.nvim_create_autocmd('FileType', {
 
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "kanagawa",
+  callback = function()
+    if vim.o.background == "light" then
+      vim.fn.system("kitty +kitten themes Kanagawa_light")
+    elseif vim.o.background == "dark" then
+      vim.fn.system("kitty +kitten themes Kanagawa_dragon")
+    else
+      vim.fn.system("kitty +kitten themes Kanagawa_dragon")
+    end
+  end,
+})
