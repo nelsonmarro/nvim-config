e1718da1f8a85f0f4d8fbc5dbc423c9d3d00dbf7
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-03 23:40:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/theme-kanagawa.lua
+++ lua/plugins/theme-kanagawa.lua
@@ -1,69 +1,35 @@
 return {
-  name = "kanagawa",
-  "rebelot/kanagawa.nvim",
-  priority = 1000,
-  lazy = false,
-  opts = {
-    compile = true, -- enable compiling the colorscheme
-    undercurl = true, -- enable undercurls
-    commentStyle = { italic = true },
-    functionStyle = { italic = true },
-    keywordStyle = { italic = true, bold = true },
-    statementStyle = { bold = true },
-    typeStyle = {},
-    transparent = false, -- do not set background color
-    dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-    terminalColors = true, -- define vim.g.terminal_color_{0,17}
-    colors = { -- add/modify theme and palette colors
-      palette = {},
-      theme = {
-        wave = {},
-        lotus = {},
-        dragon = {},
-        all = {
-          ui = {
-            bg_gutter = "none",
-          },
-        },
-      },
-    },
-    overrides = function(colors)
-      local theme = colors.theme
-      local makeDiagnosticColor = function(color)
-        local c = require("kanagawa.lib.color")
-        return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
-      end
-      return {
-        NormalFloat = { bg = "none" },
-        FloatBorder = { bg = "none" },
-        FloatTitle = { bg = "none" },
-
-        -- Popular plugins that open floats will link to NormalFloat by default;
-        -- set their background accordingly if you wish to keep them dark and borderless
-        LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-
-        DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
-        DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
-        DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
-
-        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-        PmenuSbar = { bg = theme.ui.bg_m1 },
-        PmenuThumb = { bg = theme.ui.bg_p2 },
-        DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
-      }
-    end,
-    theme = "dragon", -- Load "wave" theme when 'background' option is not set
-    background = {
-      dark = "dragon",
-      light = "lotus",
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "kanagawa-dragon",
-    },
-  },
+  -- "rebelot/kanagawa.nvim",
+  -- priority = 1000,
+  -- lazy = false,
+  -- opts = {
+  --   compile = false, -- enable compiling the colorscheme
+  --   undercurl = true, -- enable undercurls
+  --   commentStyle = { italic = true },
+  --   functionStyle = {},
+  --   keywordStyle = { italic = true },
+  --   statementStyle = { bold = true },
+  --   typeStyle = {},
+  --   transparent = false, -- do not set background color
+  --   dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+  --   terminalColors = true, -- define vim.g.terminal_color_{0,17}
+  --   colors = { -- add/modify theme and palette colors
+  --     palette = {},
+  --     theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  --   },
+  --   overrides = function(colors) -- add/modify highlights
+  --     return {}
+  --   end,
+  --   theme = "wave", -- Load "wave" theme when 'background' option is not set
+  --   background = { -- map the value of 'background' option to a theme
+  --     dark = "wave", -- try "dragon" !
+  --     light = "lotus",
+  --   },
+  -- },
+  --   {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin",
+  --   },
+  -- },
 }
