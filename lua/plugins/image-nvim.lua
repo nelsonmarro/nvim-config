fc2519b61dd4075d8f7018194c59c5396ffdb679
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-08 10:50:00 -0500

    /home/nelson/.config/nvim

---
+++ lua/plugins/image-nvim.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "3rd/image.nvim",
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {
+      processor = "magick_cli",
+    },
+  },
+  {
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add options here
+      -- or leave it empty to use the default settings
+    },
+    keys = {
+      -- suggested keymap
+      { "<leader>ip", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+    },
+  },
+}
