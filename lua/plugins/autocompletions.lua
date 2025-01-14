4065a697f5b4f983d068a723b3e0e4ad130ff87e
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-14 10:30:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/autocompletions.lua
+++ lua/plugins/autocompletions.lua
@@ -1,13 +1,28 @@
 return {
   {
-    "Jezda1337/nvim-html-css",
+    "hrsh7th/nvim-cmp",
     dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-lua/plenary.nvim",
+      "Jezda1337/nvim-html-css", -- add it as dependencies of `nvim-cmp` or standalone plugin
     },
-    config = function()
-      require("html-css"):setup()
-    end,
+    opts = {
+      sources = {
+        {
+          name = "html-css",
+          option = {
+            enable_on = { "html", "templ" }, -- html is enabled by default
+            notify = false,
+            documentation = {
+              auto_show = true, -- show documentation on select
+            },
+            -- add any external scss like one below
+            style_sheets = {
+              "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+              "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css",
+            },
+          },
+        },
+      },
+    },
   },
   {
     "hrsh7th/nvim-cmp",
