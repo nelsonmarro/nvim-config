0accb05696eba941bbf94dca1b1f5cdb115b7340
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-15 20:00:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -1,30 +1,6 @@
 return {
   -- {
-  --   'lukas-reineke/indent-blankline.nvim',
-  --   enabled = true,
-  --   event = 'lazyfile',
-  --   opts = {
-  --     indent = {
-  --       char = ' ',
-  --       tab_char = ' ',
-  --     },
-  --     scope = { show_start = false, show_end = false },
-  --     exclude = {
-  --       filetypes = {
-  --         'help',
-  --         'alpha',
-  --         'dashboard',
-  --         'neo-tree',
-  --         'trouble',
-  --         'trouble',
-  --         'lazy',
-  --         'mason',
-  --         'notify',
-  --         'toggleterm',
-  --         'lazyterm',
-  --       },
-  --     },
-  --   },
-  --   main = 'ibl',
+  "lukas-reineke/indent-blankline.nvim",
+  enabled = false,
   -- },
 }
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,4 +1,4 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  enabled = true,
+  -- 'nvim-neo-tree/neo-tree.nvim',
+  -- enabled = true,
 }
