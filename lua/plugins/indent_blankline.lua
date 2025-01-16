6decc47e936baf335a0c39b9b897236e85688bfa
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-15 20:40:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -4,6 +4,11 @@ return {
     opts = {
       indent = {
         enabled = false,
+        blank = {
+          char = " ",
+          -- char = "·",
+          hl = "SnacksIndentBlank", ---@type string|string[] hl group for blank spaces
+        },
       },
     },
   },
