75ca422cf1909cf45010caaa356e1038fa1eb5cf
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-15 20:10:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -1,6 +1,19 @@
 return {
   -- {
-  "lukas-reineke/indent-blankline.nvim",
-  enabled = false,
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   main = "ibl",
+  --   ---@module "ibl"
+  --   ---@type ibl.config
+  --   opts = {
+  --     scope = { enabled = false },
+  --   },
+  -- },
+  -- {
+  --   "snacks.nvim",
+  --   opts = {
+  --     indent = {
+  --       scope = { enabled = false },
+  --     },
+  --   },
   -- },
 }
