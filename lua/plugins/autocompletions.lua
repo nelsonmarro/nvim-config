470f2cf2d18ddf457e0667ed4ba6532a91fe2ec1
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-18 21:20:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/autocompletions.lua
+++ lua/plugins/autocompletions.lua
@@ -1,54 +1,54 @@
 return {
-  -- {
-  --   "Jezda1337/nvim-html-css",
-  --   dependencies = {
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "nvim-lua/plenary.nvim",
-  --   },
-  --   config = function()
-  --     require("html-css"):setup()
-  --   end,
-  -- },
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   dependencies = {
-  --     "hrsh7th/cmp-emoji",
-  --     "amarz45/nvim-cmp-fonts",
-  --   },
-  --   opts = function(_, opts)
-  --     -- table.insert(opts.sources, 1, {
-  --     --   name = "copilot",
-  --     --   group_index = 2,
-  --     -- })
-  --     table.insert(opts.sources, { name = "emoji" })
-  --     table.insert(opts.sources, {
-  --       name = "html-css",
-  --       option = {
-  --         enable_on = {
-  --           "html",
-  --           "javascript",
-  --           "typescript",
-  --           "javascriptreact",
-  --           "javascriptreact",
-  --           "templ",
-  --         }, -- set the file types you want the plugin to work on
-  --         file_extensions = { "css", "sass", "less" }, -- set the local filetypes from which you want to derive classes
-  --         style_sheets = {
-  --           -- example of remote styles, only css no js for now
-  --           "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
-  --         },
-  --       },
-  --     })
-  --     local format_kinds = opts.formatting.format
-  --     opts.formatting.format = function(entry, item)
-  --       if entry.source.name == "html-css" then
-  --         item.menu = entry.completion_item.menu
-  --       end
-  --       format_kinds(entry, item) -- add icons
-  --       return require("tailwindcss-colorizer-cmp").formatter(entry, item)
-  --     end
-  --   end,
-  -- },
+  {
+    "Jezda1337/nvim-html-css",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("html-css"):setup()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+      "amarz45/nvim-cmp-fonts",
+    },
+    opts = function(_, opts)
+      -- table.insert(opts.sources, 1, {
+      --   name = "copilot",
+      --   group_index = 2,
+      -- })
+      table.insert(opts.sources, { name = "emoji" })
+      table.insert(opts.sources, {
+        name = "html-css",
+        option = {
+          enable_on = {
+            "html",
+            "javascript",
+            "typescript",
+            "javascriptreact",
+            "javascriptreact",
+            "templ",
+          }, -- set the file types you want the plugin to work on
+          file_extensions = { "css", "sass", "less" }, -- set the local filetypes from which you want to derive classes
+          style_sheets = {
+            -- example of remote styles, only css no js for now
+            "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+          },
+        },
+      })
+      local format_kinds = opts.formatting.format
+      opts.formatting.format = function(entry, item)
+        if entry.source.name == "html-css" then
+          item.menu = entry.completion_item.menu
+        end
+        format_kinds(entry, item) -- add icons
+        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+      end
+    end,
+  },
 
   -- Snippets
   {
