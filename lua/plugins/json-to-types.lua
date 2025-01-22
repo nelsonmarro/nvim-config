af8f94ebd5c05180991536ffc4a61aab925956d9
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-21 22:00:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/json-to-types.lua
+++ lua/plugins/json-to-types.lua
@@ -1,18 +1,18 @@
 return {
-  {
-    "Redoxahmii/json-to-types.nvim",
-    build = "sh install.sh npm",
-    keys = {
-      {
-        "<leader>cu",
-        "<CMD>ConvertJSONtoLang typescript<CR>",
-        desc = "Convert JSON to TS",
-      },
-      {
-        "<leader>ct",
-        "<CMD>ConvertJSONtoLangBuffer typescript<CR>",
-        desc = "Convert JSON to TS in buffer",
-      },
-    },
-  },
+  -- {
+  --   "Redoxahmii/json-to-types.nvim",
+  --   build = "sh install.sh npm",
+  --   keys = {
+  --     {
+  --       "<leader>cu",
+  --       "<CMD>ConvertJSONtoLang typescript<CR>",
+  --       desc = "Convert JSON to TS",
+  --     },
+  --     {
+  --       "<leader>ct",
+  --       "<CMD>ConvertJSONtoLangBuffer typescript<CR>",
+  --       desc = "Convert JSON to TS in buffer",
+  --     },
+  --   },
+  -- },
 }
