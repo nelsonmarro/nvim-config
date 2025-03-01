88dc8f70323bcb18405e4aa5a06d27066361c236
Nelson Marro <nelsonmarro99@gmail.com>
2025-02-28 21:20:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,79 +1,79 @@
 return {
-  -- {
-  --   "catppuccin",
-  --   name = "catppuccin",
-  --   priority = 1000,
-  --   lazy = false,
-  --   opts = {
-  --     flavour = "mocha", -- latte, frappe, macchiato, mocha
-  --     background = { -- :h background
-  --       light = "latte",
-  --       dark = "mocha",
-  --     },
-  --     transparent_background = true, -- disables setting the background color.
-  --     default_integrations = true,
-  --     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-  --       comments = { "italic" }, -- Change the style of comments
-  --       conditionals = { "italic" },
-  --       loops = { "italic" },
-  --       functions = { "italic" },
-  --       keywords = { "italic" },
-  --       strings = {},
-  --       variables = {},
-  --       numbers = {},
-  --       booleans = { "italic" },
-  --       properties = {},
-  --       types = {},
-  --       operators = {},
-  --       -- miscs = {}, -- Uncomment to turn off hard-coded styles
-  --     },
-  --     integrations = {
-  --       aerial = true,
-  --       alpha = true,
-  --       overseer = true,
-  --       cmp = true,
-  --       dashboard = true,
-  --       flash = true,
-  --       gitsigns = true,
-  --       headlines = true,
-  --       illuminate = true,
-  --       indent_blankline = { enabled = true },
-  --       leap = true,
-  --       lsp_trouble = true,
-  --       mason = true,
-  --       markdown = true,
-  --       grug_far = true,
-  --       mini = true,
-  --       native_lsp = {
-  --         enabled = true,
-  --         underlines = {
-  --           errors = { "undercurl" },
-  --           hints = { "undercurl" },
-  --           warnings = { "undercurl" },
-  --           information = { "undercurl" },
-  --         },
-  --       },
-  --       navic = { enabled = true, custom_bg = "lualine" },
-  --       neotest = true,
-  --       neotree = true,
-  --       noice = true,
-  --       notify = true,
-  --       semantic_tokens = true,
-  --       treesitter = true,
-  --       treesitter_context = true,
-  --       which_key = true,
-  --       ufo = true,
-  --       telescope = {
-  --         enabled = true,
-  --       },
-  --       rainbow_delimiters = true,
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "catppuccin",
-  --   },
-  -- },
+  {
+    "catppuccin",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = true, -- disables setting the background color.
+      default_integrations = true,
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = { "italic" },
+        functions = { "italic" },
+        keywords = { "italic" },
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = { "italic" },
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+      },
+      integrations = {
+        aerial = true,
+        alpha = true,
+        overseer = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        grug_far = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+        ufo = true,
+        telescope = {
+          enabled = true,
+        },
+        rainbow_delimiters = true,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
 }
