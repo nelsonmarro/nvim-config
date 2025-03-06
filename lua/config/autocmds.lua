01d28884358d970439fd9962eaee4b2ee41d4421
Nelson Marro <nelsonmarro99@gmail.com>
2025-03-05 22:20:00 -0500

    /home/nelson/.config/nvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -79,12 +79,5 @@ vim.api.nvim_create_autocmd("ColorScheme", {
   end,
 })
 
-local prefetch = vim.api.nvim_create_augroup("prefetch", { clear = true })
-
-vim.api.nvim_create_autocmd("BufRead", {
-  group = prefetch,
-  pattern = "*.py",
-  callback = function()
-    require("cmp_tabnine"):prefetch(vim.fn.expand("%:p"))
-  end,
-})
+vim.cmd("autocmd BufEnter * set formatoptions-=cro")
+vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
