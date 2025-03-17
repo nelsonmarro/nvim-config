366cc4da87b1a65b10b590743782d9fb1500ae34
Nelson Marro <nelsonmarro99@gmail.com>
2025-03-17 07:40:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -1,26 +1,8 @@
 return {
-  -- "mg979/vim-visual-multi",
-  -- branch = "master",
-  -- lazy = false,
-  -- cond = function()
-  --   return not vim.g.vscode
-  -- end,
-  -- lazy.nvim:
-  {
-    "smoka7/multicursors.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "nvimtools/hydra.nvim",
-    },
-    opts = {},
-    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
-    keys = {
-      {
-        mode = { "v", "n" },
-        "<Leader>m",
-        "<cmd>MCstart<cr>",
-        desc = "Create a selection for selected text or word under the cursor",
-      },
-    },
-  },
+  "mg979/vim-visual-multi",
+  branch = "master",
+  lazy = false,
+  cond = function()
+    return not vim.g.vscode
+  end,
 }
