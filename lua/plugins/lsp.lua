a8bb5b6896f92dd8bb9d29e01f03f623ed290fd8
Nelson Marro <nelsonmarro99@gmail.com>
2025-05-07 18:40:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,6 +32,20 @@ return {
             format = auto_format,
           },
         },
+        golangcilsp = {
+          cmd = { "golangci-lint-langserver" },
+          root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
+          init_options = {
+            command = {
+              "golangci-lint",
+              "run",
+              "--output.json.path",
+              "stdout",
+              "--show-stats=false",
+              "--issues-exit-code=1",
+            },
+          },
+        },
         templ = {},
         html = {},
         htmx = {},
