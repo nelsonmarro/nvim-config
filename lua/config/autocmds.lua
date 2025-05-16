a11121e7efe5a9b5d4bf29f2c8cd9bee0e5b4363
Nelson Marro <nelsonmarro99@gmail.com>
2025-05-15 22:30:00 -0500

    /home/nelson/.config/nvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,25 +7,25 @@ local function augroup(name)
 end
 
 -- Crear un grupo de autocomandos para mantenerlos organizados
--- local go_lsp_augroup = vim.api.nvim_create_augroup("GoLspEnable", { clear = true })
---
--- -- Definir el autocomando
--- vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
---   group = go_lsp_augroup,
---   pattern = "*.go", -- Asegura que solo se dispare para archivos .go (aunque verificamos filetype dentro)
---   callback = function()
---     -- Verificar si el tipo de archivo es 'go'
---     if vim.bo.filetype == "go" then
---       -- Usar una variable local al buffer para asegurar que se ejecute solo una vez por buffer
---       if vim.b.golangci_lint_enabled == nil then
---         vim.lsp.enable("golangci_lint_ls")
---         vim.b.golangci_lint_enabled = true
---         -- Puedes añadir un mensaje opcional para confirmar que se habilitó
---         -- vim.notify("golangci_lint_ls habilitado para este buffer.", vim.log.levels.INFO)
---       end
---     end
---   end,
--- })
+local go_lsp_augroup = vim.api.nvim_create_augroup("GoLspEnable", { clear = true })
+
+-- Definir el autocomando
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  group = go_lsp_augroup,
+  pattern = "*.go", -- Asegura que solo se dispare para archivos .go (aunque verificamos filetype dentro)
+  callback = function()
+    -- Verificar si el tipo de archivo es 'go'
+    if vim.bo.filetype == "go" then
+      -- Usar una variable local al buffer para asegurar que se ejecute solo una vez por buffer
+      if vim.b.golangci_lint_enabled == nil then
+        vim.lsp.enable("golangci_lint_ls")
+        vim.b.golangci_lint_enabled = true
+        -- Puedes añadir un mensaje opcional para confirmar que se habilitó
+        -- vim.notify("golangci_lint_ls habilitado para este buffer.", vim.log.levels.INFO)
+      end
+    end
+  end,
+})
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "cs" },
   callback = function()
