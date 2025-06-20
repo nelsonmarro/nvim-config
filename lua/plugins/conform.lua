2332fde0f8a0edca366e500d780617fd8d23b915
Nelson Marro <nelsonmarro99@gmail.com>
2025-06-19 20:40:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,7 +3,7 @@ return {
     "conform.nvim",
     opts = function(_, opts)
       opts.formatters_by_ft.templ = { "templ" }
-      opts.formatters_by_ft.razor = { "rzls" }
+      opts.formatters_by_ft.razor = { "" }
     end,
   },
 }
