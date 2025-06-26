cca9de723c5792109e1288c2598f52e8f210f798
Nelson Marro <nelsonmarro99@gmail.com>
2025-06-26 17:50:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,25 +1,26 @@
 return {
   {
-    'stevearc/oil.nvim',
+    "stevearc/oil.nvim",
     -- Optional dependencies
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require('oil').setup {
-        columns = { 'icon' },
+      require("oil").setup({
+        delete_to_trash = true,
+        columns = { "icon" },
         keymaps = {
-          ['<C-h>'] = false,
-          ['<M-h>'] = 'actions.select_split',
+          ["<C-h>"] = false,
+          ["<M-h>"] = "actions.select_split",
         },
         view_options = {
           show_hidden = true,
         },
-      }
+      })
 
       -- Open parent directory in current window
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
       -- Open parent directory in floating window
-      vim.keymap.set('n', '<space>-', require('oil').toggle_float)
+      vim.keymap.set("n", "<space>-", require("oil").toggle_float)
     end,
   },
 }
