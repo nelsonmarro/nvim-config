b0bf017dd4b6414ce04388d29514cb3f29736013
Nelson Marro <nelsonmarro99@gmail.com>
2025-06-27 19:40:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/autocompletions.lua
+++ lua/plugins/autocompletions.lua
@@ -1,57 +1,57 @@
 return {
-  -- {
-  --   "Jezda1337/nvim-html-css",
-  --   config = function()
-  --     require("html-css"):setup()
-  --   end,
-  -- },
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   dependencies = {
-  --     "hrsh7th/cmp-emoji",
-  --     "amarz45/nvim-cmp-fonts",
-  --   },
-  --   opts = function(_, opts)
-  --     table.insert(opts.sources, 1, {
-  --       name = "copilot",
-  --       group_index = 2,
-  --     })
-  --     table.insert(opts.sources, { name = "emoji" })
-  --     table.insert(opts.sources, {
-  --       name = "html-css",
-  --       option = {
-  --         enable_on = {
-  --           "html",
-  --           "javascript",
-  --           "typescript",
-  --           "javascriptreact",
-  --           "templ",
-  --         }, -- set the file types you want the plugin to work on
-  --         file_extensions = { "css", "sass", "less" }, -- set the local filetypes from which you want to derive classes
-  --         style_sheets = {
-  --           -- example of remote styles, only css no js for now
-  --           "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
-  --           "https://cdn.jsdelivr.net/npm/@ionic/core/css/ionic.bundle.css",
-  --         },
-  --       },
-  --     })
-  --     local format_kinds = opts.formatting.format
-  --     opts.formatting.format = function(entry, item)
-  --       local source = entry.source.name
-  --       if source == "html-css" then
-  --         item.menu = "[" .. (entry.completion_item.provider .. "]" or "[html-css]")
-  --       end
-  --       local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
-  --       if color_item.abbr_hl_group then
-  --         item.kind_hl_group = color_item.abbr_hl_group
-  --         item.kind = color_item.abbr
-  --       end
-  --       format_kinds(entry, item)
-  --       return require("tailwindcss-colorizer-cmp").formatter(entry, item)
-  --     end
-  --   end,
-  -- },
-  --
+  {
+    "Jezda1337/nvim-html-css",
+    config = function()
+      require("html-css"):setup()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+      "amarz45/nvim-cmp-fonts",
+    },
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "copilot",
+        group_index = 2,
+      })
+      table.insert(opts.sources, { name = "emoji" })
+      table.insert(opts.sources, {
+        name = "html-css",
+        option = {
+          enable_on = {
+            "html",
+            "javascript",
+            "typescript",
+            "javascriptreact",
+            "templ",
+          }, -- set the file types you want the plugin to work on
+          file_extensions = { "css", "sass", "less" }, -- set the local filetypes from which you want to derive classes
+          style_sheets = {
+            -- example of remote styles, only css no js for now
+            "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+            "https://cdn.jsdelivr.net/npm/@ionic/core/css/ionic.bundle.css",
+          },
+        },
+      })
+      local format_kinds = opts.formatting.format
+      opts.formatting.format = function(entry, item)
+        local source = entry.source.name
+        if source == "html-css" then
+          item.menu = "[" .. (entry.completion_item.provider .. "]" or "[html-css]")
+        end
+        local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
+        if color_item.abbr_hl_group then
+          item.kind_hl_group = color_item.abbr_hl_group
+          item.kind = color_item.abbr
+        end
+        format_kinds(entry, item)
+        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+      end
+    end,
+  },
+
   -- -- Snippets
   {
     "L3MON4D3/LuaSnip",
