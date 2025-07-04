52d0ae89a33d7097e8fd0637db17a417355bab52
Nelson Marro <nelsonmarro99@gmail.com>
2025-07-03 19:50:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -4,46 +4,12 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    opts = function()
-      return {
-        checkbox = {
-          -- Turn on / off checkbox state rendering
-          enabled = true,
-          -- Additional modes to render checkboxes
-          render_modes = false,
-          -- Determines how icons fill the available space:
-          --  inline:  underlying text is concealed resulting in a left aligned icon
-          --  overlay: result is left padded with spaces to hide any additional text
-          position = "inline",
-          unchecked = {
-            -- Replaces '[ ]' of 'task_list_marker_unchecked'
-            icon = "󰄱 ",
-            -- Highlight for the unchecked icon
-            highlight = "RenderMarkdownUnchecked",
-            -- Highlight for item associated with unchecked checkbox
-            scope_highlight = nil,
-          },
-          checked = {
-            -- Replaces '[x]' of 'task_list_marker_checked'
-            icon = "󰱒 ",
-            -- Highlight for the checked icon
-            highlight = "RenderMarkdownChecked",
-            -- Highlight for item associated with checked checkbox
-            scope_highlight = nil,
-          },
-          -- Define custom checkbox states, more involved as they are not part of the markdown grammar
-          -- As a result this requires neovim >= 0.10.0 since it relies on 'inline' extmarks
-          -- Can specify as many additional states as you like following the 'todo' pattern below
-          --   The key in this case 'todo' is for healthcheck and to allow users to change its values
-          --   'raw':             Matched against the raw text of a 'shortcut_link'
-          --   'rendered':        Replaces the 'raw' value when rendering
-          --   'highlight':       Highlight for the 'rendered' icon
-          --   'scope_highlight': Highlight for item associated with custom checkbox
-          custom = {
-            todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
-          },
-        },
-      }
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    config = function()
+      require("obsidian").get_client().opts.ui.enable = false
+      vim.api.nvim_buf_clear_namespace(0, vim.api.nvim_get_namespaces()["ObsidianUI"], 0, -1)
+      require("render-markdown").setup({})
     end,
   },
 }
