aba36a1e63fd702bdaa66385c2432095f8b2b3ef
Nelson Marro <nelsonmarro99@gmail.com>
2025-07-16 16:20:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,13 +4,6 @@ return {
     opts = function(_, opts)
       opts.formatters_by_ft.templ = { "templ" }
       opts.formatters_by_ft.razor = { "rzls" }
-      opts.formatters.prettier = {
-        -- Prepend arguments to the prettier command
-        prepend_args = {
-          "--trailing-comma=none",
-          "--print-width=80",
-        },
-      }
     end,
   },
 }
