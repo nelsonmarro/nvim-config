71a74aba51eaf3abadb007cf2af65c761822988d
Nelson Marro <54252208+nelsonmarro@users.noreply.github.com>
2026-01-16 09:10:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -12,4 +12,23 @@ return {
       require("render-markdown").setup({})
     end,
   },
+  {
+    "nelsonmarro/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
 }
