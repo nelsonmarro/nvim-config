583f3ce3611b0b3dfe5c5b511e9451b749153481
Nelson Marro <54252208+nelsonmarro@users.noreply.github.com>
2026-01-29 15:10:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,13 +1,17 @@
 return {
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   cmd = "Copilot",
-  --   build = ":Copilot auth",
-  --   opts = {
-  --     filetypes = {
-  --       markdown = false,
-  --       help = false,
-  --     },
-  --   },
-  -- },
+  {
+    "zbirenbaum/copilot.lua",
+    depenencies = {
+      "copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
+    },
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      filetypes = {
+        markdown = false,
+        help = false,
+      },
+      copilot_node_command = "/home/nelson/.nvm/versions/node/v24.13.0/bin/node",
+    },
+  },
 }
--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -1,75 +1,97 @@
 return {
   {
     "folke/sidekick.nvim",
-    opts = function()
-      -- Accept inline suggestions or next edits
-      LazyVim.cmp.actions.ai_nes = function()
-        local Nes = require("sidekick.nes")
-        if Nes.have() and (Nes.jump() or Nes.apply()) then
-          return true
-        end
-      end
-      Snacks.toggle({
-        name = "Sidekick NES",
-        get = function()
-          return require("sidekick.nes").enabled
+    opts = {
+      -- add any options here
+      cli = {
+        mux = {
+          backend = "zellij",
+          enabled = true,
+        },
+      },
+    },
+    keys = {
+      {
+        "<tab>",
+        function()
+          -- if there is a next edit, jump to it, otherwise apply it if any
+          if not require("sidekick").nes_jump_or_apply() then
+            return "<Tab>" -- fallback to normal tab
+          end
         end,
-        set = function(state)
-          require("sidekick.nes").enable(state)
+        expr = true,
+        desc = "Goto/Apply Next Edit Suggestion",
+      },
+      {
+        "<c-.>",
+        function()
+          require("sidekick.cli").toggle()
         end,
-      }):map("<leader>uN")
-    end,
-  -- stylua: ignore
-  keys = {
-    -- nes is also useful in normal mode
-    { "<tab>", LazyVim.cmp.map({ "ai_nes" }, "<tab>"), mode = { "n" }, expr = true },
-    { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
-    {
-      "<c-.>",
-      function() require("sidekick.cli").toggle() end,
-      desc = "Sidekick Toggle",
-      mode = { "n", "t", "i", "x" },
+        desc = "Sidekick Toggle",
+        mode = { "n", "t", "i", "x" },
+      },
+      {
+        "<leader>aa",
+        function()
+          require("sidekick.cli").toggle()
+        end,
+        desc = "Sidekick Toggle CLI",
+      },
+      {
+        "<leader>as",
+        function()
+          require("sidekick.cli").select()
+        end,
+        -- Or to select only installed tools:
+        -- require("sidekick.cli").select({ filter = { installed = true } })
+        desc = "Select CLI",
+      },
+      {
+        "<leader>ad",
+        function()
+          require("sidekick.cli").close()
+        end,
+        desc = "Detach a CLI Session",
+      },
+      {
+        "<leader>at",
+        function()
+          require("sidekick.cli").send({ msg = "{this}" })
+        end,
+        mode = { "x", "n" },
+        desc = "Send This",
+      },
+      {
+        "<leader>af",
+        function()
+          require("sidekick.cli").send({ msg = "{file}" })
+        end,
+        desc = "Send File",
+      },
+      {
+        "<leader>av",
+        function()
+          require("sidekick.cli").send({ msg = "{selection}" })
+        end,
+        mode = { "x" },
+        desc = "Send Visual Selection",
+      },
+      {
+        "<leader>ap",
+        function()
+          require("sidekick.cli").prompt()
+        end,
+        mode = { "n", "x" },
+        desc = "Sidekick Select Prompt",
+      },
+      -- Example of a keybinding to open Claude directly
+      {
+        "<leader>ac",
+        function()
+          require("sidekick.cli").toggle({ name = "claude", focus = true })
+        end,
+        desc = "Sidekick Toggle Claude",
+      },
     },
-    {
-      "<leader>aa",
-      function() require("sidekick.cli").toggle() end,
-      desc = "Sidekick Toggle CLI",
-    },
-    {
-      "<leader>as",
-      function() require("sidekick.cli").select() end,
-      -- Or to select only installed tools:
-      -- require("sidekick.cli").select({ filter = { installed = true } })
-      desc = "Select CLI",
-    },
-    {
-      "<leader>ad",
-      function() require("sidekick.cli").close() end,
-      desc = "Detach a CLI Session",
-    },
-    {
-      "<leader>at",
-      function() require("sidekick.cli").send({ msg = "{this}" }) end,
-      mode = { "x", "n" },
-      desc = "Send This",
-    },
-    {
-      "<leader>af",
-      function() require("sidekick.cli").send({ msg = "{file}" }) end,
-      desc = "Send File",
-    },
-    {
-      "<leader>av",
-      function() require("sidekick.cli").send({ msg = "{selection}" }) end,
-      mode = { "x" },
-      desc = "Send Visual Selection",
-    },
-    {
-      "<leader>ap",
-      function() require("sidekick.cli").prompt() end,
-      mode = { "n", "x" },
-      desc = "Sidekick Select Prompt",
-    },
-  },
   },
 }
