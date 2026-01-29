f5af95ea58a304aa408195032a7a71ddee89e01b
Nelson Marro <54252208+nelsonmarro@users.noreply.github.com>
2026-01-29 15:30:00 -0500

    /home/nelson/.config/nvim

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,8 +2,22 @@ return {
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
+    event = "InsertEnter",
     build = ":Copilot auth",
     opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<M-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      panel = { enabled = true, auto_refresh = false },
       filetypes = {
         markdown = false,
         help = false,
